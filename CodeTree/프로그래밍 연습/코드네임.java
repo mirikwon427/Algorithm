7658302ce1f61f0e052cef3900c6ee8108a8c270
mirikwon427 <mirikwon2719@gmail.com>
2024-03-24 15:29:24 +0900

    코드네임

--- CodeTree/프로그래밍 연습/코드네임.java
+++ CodeTree/프로그래밍 연습/코드네임.java
@@ -3,7 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution16 {
+//코드네임
+public class Main {
     public static class User{
         String codeName;
         int score;
